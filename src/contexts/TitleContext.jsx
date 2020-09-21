d8bd32247c0d0dbb90133ef3ac8bafdbf536e389
rinxtzu <49005196+rinxtzu@users.noreply.github.com>
2020-09-21 14:16:35 +0300

    Updated Context

--- src/contexts/TitleContext.jsx
+++ src/contexts/TitleContext.jsx
@@ -1,6 +1,8 @@
 import React, {useState} from 'react';
 
 const ChapterContext = React.createContext({})
+//*Context for chapter
+
 
 export const ChapterProvider = ChapterContext.Provider
 export const ChapterConsumer = ChapterContext.Consumer
